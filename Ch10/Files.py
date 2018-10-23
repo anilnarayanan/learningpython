73a1345054f784674d47ef77a972489fabee9139
AKN <anilnarayanan@hotmail.com>
2018-10-23 05:48:30 +0530

    https://www.w3resource.com/python-exercises/list/

--- Ch10/Files.py
+++ Ch10/Files.py
@@ -1,7 +1,33 @@
-jabber = open("/home/anushka/Downloads/sample.txt", 'r')
+# jabber = open("/home/anushka/Downloads/sample.txt", 'r')
 
-for line in jabber:
-    if "jabber" in line.lower():
-        print(line)
+# for line in jabber:
+#     if "jabber" in line.lower():
+#         print(line)
+#
+# jabber.close()
 
-jabber.close()
+
+# with open("/home/anushka/Downloads/sample.txt", 'r') as jabber:
+#     line = jabber.readline()
+#     while(line):
+#         print(line, end='')
+#         line = jabber.readline()
+
+# with open("/home/anushka/Downloads/sample.txt", 'r') as jabber:
+#     lines = jabber.readlines()
+# # print(lines)
+#
+# for line in lines[::-1]:
+#     print(line, end='')
+
+# with open("/home/anushka/Downloads/sample.txt", 'r') as jabber:
+#     lines = jabber.read()
+#     print(lines)
+#     # for line in lines:
+#     #     print(line, end='')
+#     # lines = jabber.read()
+
+names = ["Anil", "Kumar", "Narayanan"]
+with open("/home/anushka/Downloads/name.txt", 'w') as files:
+    for name in names:
+        print(name, file=files)
