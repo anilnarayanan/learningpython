af6b01ade30eadf4ced0cdadd6a61cfaa76676cf
AKN <anilnarayanan@hotmail.com>
2018-10-23 06:07:06 +0530

    https://www.w3resource.com/python-exercises/list/

--- Challenge-Lists.py
+++ Challenge-Lists.py
@@ -2,8 +2,15 @@
 Sample List : ['abc', 'xyz', 'aba', '1221']
 Expected Result : 2"""
 
-sampleList = ['abc', 'xyz', 'aba', '1221']
+# sampleList = ['abc', 'xyz', 'aba', '1221']
+#
+# for item in sampleList:
+#     if len(item) >= 2 and item[0] == item[-1]:
+#         print(sampleList.index(item))
 
-for item in sampleList:
-    if len(item) >= 2 and item[0] == item[-1]:
-        print(sampleList.index(item))
+# Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples. Go to the editor
+# Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+sampleList = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+
